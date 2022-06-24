d63b5bfa62b0f459053e351bacbfce1298dbf099
Luis Branco <luisjbrancog@gmail.com>
2022-06-24 14:14:41 +0100

    Finished the DOM section

--- DOM/index.js
+++ DOM/index.js
@@ -1,1 +1,6 @@
-alert("Hello");
+// alert("Hello");
+
+// document.getElementsByTagName("li")[2].style.color = "purple";
+
+// console.log(document.getElementsByClassName("btn"));
+// document.getElementsByClassName("btn")[0].style.color = "red";
