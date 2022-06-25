4d4425dab9995016b60164919a489cd37654cae6
Luis Branco <luisjbrancog@gmail.com>
2022-06-25 11:50:51 +0100

    Introduction to Node.js

---
+++ intro_to_node/index.js
@@ -0,0 +1,20 @@
+/*
+// Internal module
+const fs = require("fs");
+
+// COpy a file
+fs.copyFileSync("file1.txt", "file2.txt");
+
+// External module
+const superheroes = require("superheroes");
+
+const mySuperHeroName = superheroes.random();
+
+console.log(mySuperHeroName);
+
+const supervillains = require("supervillains");
+
+const superVillainName = supervillains.random();
+
+console.log(superVillainName);
+*/
