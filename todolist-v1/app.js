8167f9a76967cbe2439cfb0a98e30b407dc17531
Luis Branco <luisjbrancog@gmail.com>
2022-06-28 18:22:13 +0100

    Modified structure

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -6,7 +6,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello");
+  const today = new Date();
+  const currentDay = today.getDay();
+
+  if (currentDay === 6 || currentDay === 0) {
+    res.send("<h1>Yay it's the weekend!</h1>");
+  } else {
+    res.sendFile(__dirname + "/index.html");
+  }
 });
 
 app.listen(3000, function () {
