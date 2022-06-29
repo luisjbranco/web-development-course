005bfdc88ba4565d86411f7775cb87b077d82a9f
Luis Branco <luisjbrancog@gmail.com>
2022-06-29 19:13:25 +0100

    Completed the EJS for templating

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,18 +2,45 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-app.get("/", function (req, res) {
-  const today = new Date();
-  const currentDay = today.getDay();
+const items = [];
+const workItems = [];
 
-  if (currentDay === 6 || currentDay === 0) {
-    res.send("<h1>Yay it's the weekend!</h1>");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  let day = date.getDate();
+  res.render("list", {
+    listTitle: day,
+    newListItems: items,
+  });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
   } else {
-    res.sendFile(__dirname + "/index.html");
+    items.push(item);
+    res.redirect("/");
   }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, function () {
